39108648df2a6a2dbbbbb82e49a6601fcf0e1726
Jakob Laback <jlaback@student.tgm.ac.at>
2018-10-20 23:48:21 +0200

    setKeyword teilweise

--- src/KeywordCipher.java
+++ src/KeywordCipher.java
@@ -9,11 +9,24 @@ public class KeywordCipher extends MonoAlphabeticCipher{
 	 * @param passwort
 	 */
 	
-	public KeywordCipher(String passwort) {
+	public KeywordCipher(String keyword) {
 		this.passwort = passwort;
 	}
 	
-	public void setKeyword(String passwort) { //später--> throws ...Exception
+	public void setKeyword(String keyword) { //später--> throws ...Exception
+		//Exception-Abfrage
+		
+		this.passwort = keyword.toUpperCase();
+		String s = "";
+		
+		for (int i = 0; i < this.passwort.length(); i++) {
+			s += this.passwort.charAt(i);
+		}
+		
+		for (int i = 0; i < 30; i++) {
+			s += (char) (i+21);
+		}
+		
 		
 	}
 }
