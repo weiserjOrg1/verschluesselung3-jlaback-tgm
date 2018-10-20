0a8a1155aeb9dd13af23ee45bee4f2e06cb4241d
Jakob Laback <jlaback@student.tgm.ac.at>
2018-10-20 23:30:00 +0200

    Methodenköpfe + Konstruktor

---
+++ src/TranspositionCipher.java
@@ -0,0 +1,26 @@
+
+public class TranspositionCipher implements Cipher{
+	
+	/**
+	 * Konstruktor bekommt transpositionLevel
+	 * @param transpositionLevel
+	 */
+	public TranspositionCipher(int transpositionLevel) {
+		
+	}
+	/**
+	 * setzt Transposition-Level
+	 * @param transpositionLevel
+	 */
+	public setTranspositionLevel(int transpositionLevel) {
+		
+	}
+
+	public String encrypt(String text) {
+		
+	}
+	
+	public String decrypt(String text) {
+		
+	}
+}
